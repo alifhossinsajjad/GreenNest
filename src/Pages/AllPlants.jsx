44eccdcbcba3b0bff6a93c4b146f0a06f1839e38
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-10-23 22:47:36 +0600

    fix the component name

--- src/Pages/AllPlants.jsx
+++ src/Pages/AllPlants.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLoaderData } from "react-router";
-import AllPlantsCard from "../Components/AllPlantsCard.jsx";
+
 import { FaArrowLeft } from "react-icons/fa";
+import AllPlantsCard from "../Components/AllPlantsCard";
 
 
 const AllPlants = () => {
